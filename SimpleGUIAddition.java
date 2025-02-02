1955ba59a521efb86dd3316322e25b2bd541c80c
Hir2wa <alainfabricehirwa@gmail.com>
2025-02-02 18:51:37 +0200

    ..

---
+++ SimpleGUIAddition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class SimpleGUIAddition {
+    public static void main(String[] args) {
+        
+        int num1,num2,sum;
+        String number1,number2;
+        Scanner input = new Scanner(System.in);
+        JOptionPane.showMessageDialog(null, "Enter First Number");
+          number1= JOptionPane.showInputDialog("Enter the  first number");
+    }
+}
