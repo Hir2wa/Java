28feba7da5e28269e909b0a4592b9b602aeeff85
Hir2wa <alainfabricehirwa@gmail.com>
2025-02-02 19:05:29 +0200

    ..

--- SimpleGUIAddition.java
+++ SimpleGUIAddition.java
@@ -9,6 +9,13 @@ public class SimpleGUIAddition {
         String number1,number2;
         Scanner input = new Scanner(System.in);
       //  JOptionPane.showMessageDialog(null, "Enter First Number");
-          number1= JOptionPane.showInputDialog("Enter the  first number");
+          number1= JOptionPane.showInputDialog("Enter The  First Number");
+          number2= JOptionPane.showInputDialog("Enter The Second Number");
+        
+          num1 = Integer.parseInt(number1);
+          num2= Integer.parseInt(number2);
+          sum= num1 + num2;
+          JOptionPane.showMessageDialog(null, "The Sum =" +sum , "Result",JOptionPane.ERROR_MESSAGE);
+        
     }
 }
